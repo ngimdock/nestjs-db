ba8abf8262647f17eb9623b81f15634cfbc8f5ef
ngimdock <ngimdockzemfack@gmail.com>
2022-11-11 17:25:14 +0100

    Setup the transactions

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
-import { User } from './users.entity';
+// import { User } from './users.entity';
+import { usersSchema } from './users.schema';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([usersSchema])],
   exports: [TypeOrmModule],
   controllers: [UsersController],
   providers: [UsersService],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { User } from './users.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
+    private datasource: DataSource,
     @InjectRepository(User) private usersRepository: Repository<User>,
   ) {}
 
@@ -19,5 +20,30 @@ export class UsersService {
 
   async remove(id: number): Promise<void> {
     await this.usersRepository.delete(id);
+  }
+
+  async createMany(users: User[]) {
+    const queryRunner = this.datasource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.manager.save(users[0]);
+      await queryRunner.manager.save(users[1]);
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  async createMany2(users: User[]) {
+    this.datasource.transaction(async (manager) => {
+      await manager.save(users[0]);
+      await manager.save(users[1]);
+    });
   }
 }
